5f4eda0cbccc627b7bbd1ed6832c9d087b0505c1
noCoinPerSoul <anton0886@gmail.com>
2020-05-16 19:04:10 +0300

    add properties

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
+const App = ({ name }) => {
     return ( < div >
         <
-        h1 > Hello World! < /h1 >  < /div > )
+        h1 > Hello World! < /h1 > <
+        p > Your name { name } < /p> < /div > )
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( < App / > ,
+ReactDOM.render( < App name = 'Anton' / > ,
     document.getElementById('root')
 );
 
