cb4a3048315e0659256cd5fe9cbf471b1beaea77
noCoinPerSoul <anton0886@gmail.com>
2020-05-30 02:11:38 +0300

    ComitFromClasswork5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { getAlbumID, getAlbumImages } from "./utilites/API";
 
-const App = ({ name }) => {
-    return ( < div >
-        <
-        h1 > Hello World! < /h1 > <
-        p > Your name { name } < /p> < /div > )
+const App = () => {
+    const [albumIdArray, setAlbumIdArray] = React.useState();
+    const [albumOfImages, setAlbumOfImages] = React.useState();
+    useEffect(() => {
+        getAlbumID()
+            .then(res => setAlbumIdArray(res.data.data));
+    }, []);
+
+    useEffect(() => {
+        let tmpAlbumIdFromImages = [];
+        albumIdArray && albumIdArray.map((hash) => {
+            getAlbumImages(hash)
+                .then(res => tmpAlbumIdFromImages.push(
+                    {
+                        id: hash,
+                        images: res.data.data,
+                    }
+                ));
+        });
+        albumIdArray && setAlbumOfImages(tmpAlbumIdFromImages);
+        console.log(tmpAlbumIdFromImages);
+    }, [albumIdArray]);
+
+    return <div>
+
+    </div>
 };
 
 export default App;
---
+++ src/utilites/API.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API = "https://api.imgur.com/3";
+
+const PARAMS = {
+    headers: {
+        Authorization: "Client-ID 121926766c8002b"
+    }
+};
+
+const getAlbumID = () => {
+    return axios.get(`${API}/account/anton0886/albums/ids/0`, PARAMS);
+};
+
+const getAlbumImages = (hash) => {
+    return axios.get(`${API}/album/${hash}/images`, PARAMS);
+};
+
+export { getAlbumID, getAlbumImages };
