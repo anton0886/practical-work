1906ccc3b5be13b52d976ff98a9f48da14da7699
noCoinPerSoul <anton0886@gmail.com>
2020-06-06 15:19:06 +0300

    Start

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 
 const App = ({ name }) => {
-    return ( < div >
-        <
-        h1 > Hello World! < /h1 > <
-        p > Your name { name } < /p> < /div > )
+    return < div >
+        <h1 > Hello World! </h1>
+        <p> Your name {name} </p>
+        </div>
 };
 
 export default App;
